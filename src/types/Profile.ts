0a37e531373477b42f63dcd8bb5bf3cb7ff6543a
joseph <joseph.perrotta@ymail.com>
2025-05-12 10:50:05 +0200

    Fix Profile type error by making avatar_url optional

---
+++ src/types/Profile.ts
@@ -0,0 +1,13 @@
+export type Profile = {
+  about_me: string;
+  age: number;
+  created_at: string;
+  current_city: string;
+  full_name: string;
+  id: string;
+  move_in_city: string;
+  nationality: string;
+  university: string;
+  updated_at: string;
+  avatar_url?: string;
+}; 
